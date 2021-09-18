bde34e9ea3d86cd74c2f000fe274b81ed418b4b9
Adam Orczyk <adam.orczyk@gmail.com>
2021-09-18 20:23:55 +0200

    Aktualizacja main.ts

--- main.ts
+++ main.ts
@@ -11,7 +11,6 @@
 //% category="MakerBit"
 namespace makerbit {
     let irLed: InfraredLed;
-    let scheduler: Scheduler;
     export let debug: boolean = false;
 
     function splitToBulks(arr: string[], bulkSize = 20) {
@@ -53,6 +52,10 @@ namespace makerbit {
                 }
                 const end = input.runningTimeMicros();
                 this.waitCorrection = Math.idiv(end - start - runs * 2, runs * 2);
+
+                console.log('calibration')
+                console.log(end - start)
+                console.log(this.waitCorrection)
             }
 
             // Insert a pause between callibration and first message
@@ -81,15 +84,16 @@ namespace makerbit {
             let nibble2 = (address << 3) | mode;
             let lrc = 15 ^ nibble1 ^ nibble2 ^ data;
 
-            // let out = (((nibble1 << 4) + nibble2 << 4) + data << 4) + lrc;
             let out = (nibble1 << 12) | (nibble2 << 8) | (data << 4) | lrc;
 
             let bits = createBinaryString(out).split('');
 
             const PF_MARK_BIT = 158;
-            const PF_LOW_BIT = 421 - PF_MARK_BIT;
-            const PF_HIGH_BIT = 711 - PF_MARK_BIT;
-            const PF_START_BIT = 1184 - PF_MARK_BIT;
+            const PF_LOW_BIT = 421 - PF_MARK_BIT - this.waitCorrection;
+            const PF_HIGH_BIT = 711 - PF_MARK_BIT - this.waitCorrection;
+            const PF_START_BIT = 1184 - PF_MARK_BIT - this.waitCorrection;
+
+            let start = input.runningTimeMicros();
 
             this.transmitBit(PF_MARK_BIT, PF_START_BIT);
 
@@ -104,80 +108,67 @@ namespace makerbit {
             this.transmitBit(PF_MARK_BIT, PF_START_BIT);
 
             // let pasue = Math.floor(4 * this.messageLength / 1000);
-            let pasue = 20;
+            // basic.pause(pasue)
+
             if (debug) {
                 console.log(JSON.stringify(splitToBulks(bits, 4)))
-                console.log(this.messageLength)
-                console.log(pasue)
+                console.log(this.messageLength / 1000)
+                console.log((input.runningTimeMicros() - start) / 1000)
             }
-
-            basic.pause(pasue)
         }
     }
 
-    class Scheduler {
-        // --- IR signal delays ---
-        // Minimal delay every ir signal
-        private irSignalDelay: number;
-        // Minimal delay every signal to specified channel
-        private irChannelSignalDelay: number;
-        // Minimal delay every signal to specified channel output (red or blue)
-        private irOutputSignalDelay: number;
+    // --- Scheduler ---
 
-        private channels: number[][] = [
-            [0, 0],
-            [0, 0],
-            [0, 0],
-            [0, 0],
-        ];
+    // --- IR signal delays ---
+    // Minimal delay every ir signal
+    export let signalDelay: number = 20;
+    // Minimal delay every signal to specified channel
+    export let channelDelay: number = 20;
+    // Minimal delay every signal to specified channel output (red or blue)
+    export let outputDelay: number = 600;
 
-        private irLastSignalTime: number = 0;
-        private channelsIr: number[] = [0, 0, 0, 0];
-        private scheduledEvents: number = 0;
+    let signalTime: number = 0;
+    let channelTime: number[] = [0, 0, 0, 0];
+    let outputTime: number[][] = [
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+    ];
 
-        constructor(irSignalDelay: number = 20, irChannelSignalDelay: number = 20, irOutputSignalDelay: number = 600,) {
-            this.irSignalDelay = irSignalDelay;
-            this.irChannelSignalDelay = irChannelSignalDelay;
-            this.irOutputSignalDelay = irOutputSignalDelay;
-        }
-
-        schedule(channel: number, output: number, handler: () => void) {
-            if (this.scheduledEvents > 6) {
-                while (this.scheduledEvents > 7) {
-                    basic.pause(20);
-                }
-            }
-
-            this.scheduledEvents += 1;
-
-            let time = input.runningTime();
-
-            let scheduleTime = [
-                this.irLastSignalTime + this.irSignalDelay,
-                this.irChannelSignalDelay + this.channelsIr[channel],
-                this.irOutputSignalDelay + this.channels[channel][output],
-                time
-            ].sort(function compareNumbers(a, b) {
-                return b - a
-            })[0];
-
-            this.irLastSignalTime = scheduleTime;
-            this.channelsIr[channel] = scheduleTime;
-            this.channels[channel][output] = scheduleTime;
-
-            let pauseTime = scheduleTime - time;
-
-            control.inBackground(() => {
-                if (pauseTime > 0) {
-                    basic.pause(pauseTime)
-                }
-
-                this.scheduledEvents -= 1;
-
-                handler();
-            })
-        }
+    function compareNumbers(a: number, b: number) {
+        return b - a
     }
+
+    function schedule(channel: number, output: number, handler: () => void) {
+        let time = input.runningTime();
+
+        let scheduleTime = [
+            signalTime + signalDelay,
+            channelTime[channel] + channelDelay,
+            outputTime[channel][output] + outputDelay,
+            time
+        ].sort(compareNumbers)[0];
+
+        let pauseTime = scheduleTime - time;
+
+        if (pauseTime > 0) {
+            basic.pause(pauseTime)
+        }
+
+        serial.writeNumbers([time, scheduleTime, input.runningTime()])
+
+        handler();
+
+        let now = input.runningTime();
+
+        signalTime = now;
+        channelTime[channel] = now;
+        outputTime[channel][output] = now;
+    }
+
+    // ---
 
     /**
      * Connects to the IR-emitting LED at the specified pin.
@@ -192,23 +183,22 @@ namespace makerbit {
     //% weight=90
     export function connectIrSenderLed(pin: AnalogPin): void {
         irLed = new InfraredLed(pin);
-        scheduler = new Scheduler();
     }
 
     export function sendPf(): void {
         // // irLed.sendPfDatagram(0, 0, 0, 1, 101);
-        // irLed.sendPfDatagram(0, 0, 0, 100, 111);
+        irLed.sendPfDatagram(0, 0, 0, 100, 111);
         // irLed.sendPfDatagram(0, 0, 0, 101, 111);
         // basic.pause(1000);
         // irLed.sendPfDatagram(0, 0, 0, 100, 1000);
         // irLed.sendPfDatagram(0, 0, 0, 101, 1000);
         // // irLed.sendPf(0, 0, 0, 1, 101);
 
-        scheduler.schedule(0, 0, () => irLed.sendPfDatagram(0, 0, 0, 100, 111))
-        scheduler.schedule(0, 1, () => irLed.sendPfDatagram(0, 0, 0, 101, 111))
-        basic.pause(1000);
-        scheduler.schedule(0, 0, () => irLed.sendPfDatagram(0, 0, 0, 100, 1000))
-        scheduler.schedule(0, 1, () => irLed.sendPfDatagram(0, 0, 0, 101, 1000))
+        // schedule(0, 0, () => irLed.sendPfDatagram(0, 0, 0, 100, 111))
+        // schedule(0, 1, () => irLed.sendPfDatagram(0, 0, 0, 101, 111))
+        // basic.pause(1000);
+        // schedule(0, 0, () => irLed.sendPfDatagram(0, 0, 0, 100, 1000))
+        // schedule(0, 1, () => irLed.sendPfDatagram(0, 0, 0, 101, 1000))
     }
 }
 
