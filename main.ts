1ed484b68d8e15500f4c2d1d34bdb3a779113313
Adam Orczyk <adam.orczyk@gmail.com>
2021-09-23 17:17:08 +0200

    Aktualizacja main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,78 @@
-/**
- * TODO
- * - ponawianie tej samej wiadomości (nadajnik ponawia 4x) ?
- * - scheduler - prawidłowe odstępy pomiędzy kolejnymi poleceniami (tak w moim wrapperze)
- */
+const enum Channel {
+    //% block="1"
+    Channel1 = 0,
+    //% block="2"
+    Channel2 = 1,
+    //% block="3"
+    Channel3 = 2,
+    //% block="4"
+    Channel4 = 3,
+}
 
-/**
- * MakerBit
- */
-//% color=#0fbc11 icon="\u272a" block="PF Transmitter"
+const enum Output {
+    Red = 0,
+    Blue = 1
+}
+
+const enum SingleOutput {
+    //% block="Float"
+    Float = 0b1000000,
+    //% block="Forward 1"
+    Forward1 = 0b1000001,
+    //% block="Forward 2"
+    Forward2 = 0b1000010,
+    //% block="Forward 3"
+    Forward3 = 0b1000011,
+    //% block="Forward 4"
+    Forward4 = 0b1000100,
+    //% block="Forward 5"
+    Forward5 = 0b1000101,
+    //% block="Forward 6"
+    Forward6 = 0b1000110,
+    //% block="Forward 7"
+    Forward7 = 0b1000111,
+    //% block="Brake then float"
+    BrakeThenFloat = 0b1000111,
+    //% block="Backward 7"
+    Backward7 = 0b1001001,
+    //% block="Backward 6"
+    Backward6 = 0b1001010,
+    //% block="Backward 5"
+    Backward5 = 0b1001011,
+    //% block="Backward 4"
+    Backward4 = 0b1001100,
+    //% block="Backward 3"
+    Backward3 = 0b1001101,
+    //% block="Backward 2"
+    Backward2 = 0b1001110,
+    //% block="Backward 1"
+    Backward1 = 0b1001111,
+    //% block="Increment PWM"
+    IncrementPWM = 0b1100100,
+    //% block="Decrement PWM"
+    DecrementPWM = 0b1100101,
+    //% block="Full forward"
+    FullForward = 0b1100110,
+    //% block="Full backward"
+    FullBackward = 0b1100111,
+}
+
+const enum ComboDirect {
+    //% block="Float"
+    Float = 0b00,
+    //% block="Forward"
+    Forward = 0b01,
+    //% block="Backward"
+    Backward = 0b10,
+    //% block="Brake then float"
+    BrakeThenFloat = 0b11,
+}
+
+//% color=#f68420 icon="\uf1eb" block="PF Transmitter"
 namespace pfTransmitter {
     let irLed: InfraredLed;
     export let debug: boolean = false;
+    export let repeatCommandTime: number = 1000;
 
     function splitToBulks(arr: number[], bulkSize = 20) {
         const bulks = [];
@@ -167,8 +229,7 @@ namespace pfTransmitter {
      * Connects to the IR-emitting LED at the specified pin.
      * @param pin IR LED pin, eg: AnalogPin.P0
      */
-    //% subcategory="IR Sender"
-    //% blockId="makerbit_infrared_sender_connect"
+    //% blockId="pf_transmitter_infrared_sender_connect"
     //% block="connect IR sender LED at pin %pin"
     //% pin.fieldEditor="gridpicker"
     //% pin.fieldOptions.columns=4
@@ -204,76 +265,98 @@ namespace pfTransmitter {
                 return false;
             }
             return true;
-        }, 1000)
+        }, repeatCommandTime)
     }
-}
 
-enum Channel {
-    'Ch_1' = 0,
-    'Ch_2' = 1,
-    'Ch_3' = 2,
-    'Ch_4' = 3,
-}
+    const enum ComboPWM {
+        //% block="Float"
+        Float = 0b0000,
+        //% block="Forward 1"
+        Forward1 = 0b0001,
+        //% block="Forward 2"
+        Forward2 = 0b0010,
+        //% block="Forward 3"
+        Forward3 = 0b0011,
+        //% block="Forward 4"
+        Forward4 = 0b0100,
+        //% block="Forward 5"
+        Forward5 = 0b0101,
+        //% block="Forward 6"
+        Forward6 = 0b0110,
+        //% block="Forward 7"
+        Forward7 = 0b0111,
+        //% block="Brake then float"
+        BrakeThenFloat = 0b1000,
+        //% block="Backward 7"
+        Backward7 = 0b1001,
+        //% block="Backward 6"
+        Backward6 = 0b1010,
+        //% block="Backward 5"
+        Backward5 = 0b1011,
+        //% block="Backward 4"
+        Backward4 = 0b1100,
+        //% block="Backward 3"
+        Backward3 = 0b1101,
+        //% block="Backward 2"
+        Backward2 = 0b1110,
+        //% block="Backward 1"
+        Backward1 = 0b1111,
+    }
 
-enum Output {
-    'Red' = 0,
-    'Blue' = 1
-}
+    export function comboPWMMode(channel: Channel, red: ComboPWM, blue: ComboPWM) {
+        let command: number = (blue << 4) | red;
+        let datagram = ((0b0100 | channel) << 8) | command;
+        lastCommand[channel] = command;
 
-enum SingleOutput {
-    'Float' = 0b1000000,
-    'Forward_1' = 0b1000001,
-    'Forward_2' = 0b1000010,
-    'Forward_3' = 0b1000011,
-    'Forward_4' = 0b1000100,
-    'Forward_5' = 0b1000101,
-    'Forward_6' = 0b1000110,
-    'Forward_7' = 0b1000111,
-    'Brake then float' = 0b1000111,
-    'Backward_7' = 0b1001001,
-    'Backward_6' = 0b1001010,
-    'Backward_5' = 0b1001011,
-    'Backward_4' = 0b1001100,
-    'Backward_3' = 0b1001101,
-    'Backward_2' = 0b1001110,
-    'Backward_1' = 0b1001111,
-    'Increment_PWM' = 0b1100100,
-    'Decrement_PWM' = 0b1100101,
-    'Full_forward' = 0b1100110,
-    'Full_backward' = 0b1100111,
-}
+        if (command == lastCommand[channel]) {
+            return;
+        }
 
-enum ComboDirect {
-    'Float' = 0b00,
-    'Forward' = 0b01,
-    'Backward' = 0b10,
-    'Brake_then_float' = 0b11,
+        setInterval(() => {
+            if (command == lastCommand[channel]) {
+                sendPacket(datagram);
+
+                if (command == 0) {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }, repeatCommandTime)
+    }
+
+    export function play(commands: number[][]){
+        commands.forEach((command: number[]) => {
+            sendPacket(command[0]);
+            basic.pause(command[2])
+        })
+    }
 }
 
 pfTransmitter.connectIrSenderLed(AnalogPin.P0)
 pfTransmitter.debug = true;
 
 input.onButtonPressed(Button.A, function () {
-    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Forward_1)
-    // pfTransmitter.singleOutputMode(0, 1, SingleOutput.Forward_1)
+    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Forward1)
+    // pfTransmitter.singleOutputMode(0, 1, SingleOutput.Forward1)
     // basic.pause(1000);
-    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Forward_2)
-    // pfTransmitter.singleOutputMode(0, 1, SingleOutput.Forward_2)
+    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Forward2)
+    // pfTransmitter.singleOutputMode(0, 1, SingleOutput.Forward2)
     // basic.pause(1000);
-    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Forward_3)
-    // pfTransmitter.singleOutputMode(0, 1, SingleOutput.Forward_3)
+    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Forward3)
+    // pfTransmitter.singleOutputMode(0, 1, SingleOutput.Forward3)
     // basic.pause(1000);
-    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Forward_4)
-    // pfTransmitter.singleOutputMode(0, 1, SingleOutput.Forward_4)
+    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Forward4)
+    // pfTransmitter.singleOutputMode(0, 1, SingleOutput.Forward4)
     // basic.pause(1000);
-    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Forward_5)
-    // pfTransmitter.singleOutputMode(0, 1, SingleOutput.Forward_5)
+    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Forward5)
+    // pfTransmitter.singleOutputMode(0, 1, SingleOutput.Forward5)
     // basic.pause(1000);
-    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Forward_6)
-    // pfTransmitter.singleOutputMode(0, 1, SingleOutput.Forward_6)
+    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Forward6)
+    // pfTransmitter.singleOutputMode(0, 1, SingleOutput.Forward6)
     // basic.pause(1000);
-    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Forward_7)
-    // pfTransmitter.singleOutputMode(0, 1, SingleOutput.Forward_7)
+    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Forward7)
+    // pfTransmitter.singleOutputMode(0, 1, SingleOutput.Forward7)
     // basic.pause(1000);
     // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Float)
     // pfTransmitter.singleOutputMode(0, 1, SingleOutput.Float)
@@ -283,5 +366,5 @@ input.onButtonPressed(Button.A, function () {
 
 input.onButtonPressed(Button.B, function () {
     pfTransmitter.comboDirectMode(0, ComboDirect.Float, ComboDirect.Float)
-    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.Full_backward)
+    // pfTransmitter.singleOutputMode(0, 0, SingleOutput.FullBackward)
 })
